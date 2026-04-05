8c8a4f976ef69c259ccc7b9ce90d0f3503c89753
regtop6800@gmail.com <regtop6800@gmail.com>
2026-04-05 15:18:58 +0500

    Github

--- github/main.js
+++ github/main.js
@@ -3,17 +3,44 @@ let searchBtn = document.getElementById("searchBtn");
 let card = document.getElementById("card");
 let load = document.getElementById("load");
 
-
+let dataCard
 
 async function getCard() {
     card.classList.add("none");
     load.classList.remove("none");
 
-    const response = await fethch("https://api.github.com/users/KrasnovDaiil1")
+    const response = await fetch(`https://api.github.com/users/${searchValue.value}`);
     if (response.ok) {
         dataCard = await response.json()
         console.log(dataCard)
+        generateCard()
     }
+    load.classList.add("none");
 }
 
+
 searchBtn.addEventListener("click", getCard);
+
+function generateCard() {
+card.innerHTML = `
+    <img src="${dataCard.avatar_url}">
+        <h1>${dataCard.login}</h1>
+        <p>${dataCard.bio}</p>
+        <nav>
+            <div>
+                <i class="fas fa-map-marker-alt" style="color:yellow"></i>
+                <span>${dataCard.location}</span>
+            </div>
+            <div>
+                <i class="fad fa-book" style="color: green"></i>
+                <span>${dataCard.public_repos}</span>
+            </div>
+            <div>
+                <i class="fas fa-heart" style="color: red"></i>
+                <span>${dataCard.followers}</span>
+            </div>
+        </nav>
+        <a href="${dataCard.url}">Перейти</a>
+    `
+    card.classList.remove("none");
+}
